89112cf0a411b739f10f8805a4e859f44fd17679
Prateek Dixit <prateekdixit252006@gmail.com>
2026-06-17 18:34:52 +0530

    Updated the todolist v-3 and replaced usestate with useref

--- 4.ToDoList-V-3/src/Components/AddTodo.jsx
+++ 4.ToDoList-V-3/src/Components/AddTodo.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 import styles from './Row.module.css';
 import { BiMessageAdd } from "react-icons/bi";
 
 
 function AddTodo({fn}) {
-  let [name,setM] = useState("");
-  let [date,setD] = useState("");
+  let name1 = useRef();
+  let date1 = useRef();
+  function handleClick(event) {
+    event.preventDefault();
+    fn(name1.current.value,date1.current.value);
+    name1.current.value = "";
+    date1.current.value ="";
+  }
   return (
-    <div className={`row ${styles['kg-row']}`}>
-        <div className="col-sm-6"><input type="text" placeholder="Enter todo here" onChange={(event) => setM(event.target.value)} value={name}/></div>
-        <div className="col-sm-3"><input type="date" onChange={(event) => setD(event.target.value)} value={date}/></div>
-        <div className="col-sm-2"><button onClick={()=>{
-          fn(name,date);
-          setD("");
-          setM("");
-          }} type="button" className={`btn btn-success ${styles['kg-button']}`}><BiMessageAdd /></button></div>
-      </div>
+    <form className={`row ${styles['kg-row']}`} onSubmit = {handleClick}
+    >
+        <div className="col-sm-6"><input type="text" placeholder="Enter todo here" ref ={name1}/></div>
+        <div className="col-sm-3"><input type="date" ref={date1}/></div>
+        <div className="col-sm-2"><button  className={`btn btn-success ${styles['kg-button']}`}
+          ><BiMessageAdd /></button></div>
+      </form>
   );
 }
 
